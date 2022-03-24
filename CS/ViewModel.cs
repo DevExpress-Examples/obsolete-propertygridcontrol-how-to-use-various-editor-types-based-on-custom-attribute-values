ef676c1f9b119c4fab2e7f9dac99bc9b5584f4a5
dxIvan <38530056+ivansupport@users.noreply.github.com>
2022-03-24 17:44:43 +0400

    update

--- CS/ViewModel.cs
+++ CS/ViewModel.cs
@@ -1,20 +1,17 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.ComponentModel;
-using System.Linq;
-using System.Text;
 
 namespace DXSample {
     public class ViewModel {
         public Department Department { get; set; }
         public ViewModel() {
             Department = new Department { Name = "Department" };
-            Department.Employees.Add(new Employee { Name = "Jett Mitchell" });
-            Department.Employees.Add(new Employee { Name = "Garrick Stiedemann DVM" });
-            Department.Employees.Add(new Employee { Name = "Hettie Runte" });
+            Department.Employees.Add(new Employee { ButtonEdit = "Jett Mitchell", });
+            Department.Employees.Add(new Employee { ButtonEdit = "Garrick Stiedemann DVM" });
+            Department.Employees.Add(new Employee { ButtonEdit = "Hettie Runte" });
         }
     }
+    [TypeConverter(typeof(ExpandableObjectConverter))]
     public class Department {
         public Department() {
             Employees = new BindingList<Employee>();
@@ -22,9 +19,10 @@ namespace DXSample {
         public string Name { get; set; }
         public BindingList<Employee> Employees { get; set; }
     }
+    [TypeConverter(typeof(ExpandableObjectConverter))]
     public class Employee {
-        [CustomEditorAttribute("ButtonEdit")]
-        public string Name { get; set; }
+        [CustomEditor("ButtonEdit")]
+        public string ButtonEdit { get; set; }
     }
     public class CustomEditorAttribute : Attribute {
         public string Name { get; private set; }
--- VB/Helpers.vb
+++ VB/Helpers.vb
@@ -21,21 +21,20 @@ Namespace DXSample
         Public Function Convert(ByVal value As Object, ByVal targetType As Type, ByVal parameter As Object, ByVal culture As Globalization.CultureInfo) As Object Implements IValueConverter.Convert
             Dim descriptor As Descriptor = TryCast(value, Descriptor)
             If descriptor IsNot Nothing Then
-                If descriptor.PropertyDescriptor Is Nothing Then
-                    value = descriptor.Data
-                Else
-                    value = descriptor.PropertyDescriptor.GetValue(descriptor.Data)
-                    If TypeOf value Is IEnumerable Then
-                        Dim i As Integer = 0
-                        Return CType(value, IEnumerable).Cast(Of Object)().[Select](Function(x) New Descriptor() With {.Path = "[" & Math.Min(Threading.Interlocked.Increment(i), i - 1) & "]", .Data = x, .IsCollection = True}).ToList()
-                    End If
-                End If
-            ElseIf value Is Nothing Then
+                value = If(descriptor.PropertyDescriptor Is Nothing, descriptor.Data, descriptor.PropertyDescriptor.GetValue(descriptor.Data))
+            End If
+            Dim collection As IEnumerable = TryCast(value, IEnumerable)
+            If collection IsNot Nothing Then
+                Dim i = 0
+                Return collection.Cast(Of Object)().Select(Function(x)
+                                                               Return New Descriptor("[" + i.ToString() + "]", x, Nothing)
+                                                               i += 1
+                                                           End Function)
+            End If
+            If value Is Nothing Then
                 Return Nothing
             End If
-
-            Dim props = TypeDescriptor.GetProperties(value)
-            Return props.Cast(Of PropertyDescriptor)().[Select](Function(x) New Descriptor With {.PropertyDescriptor = x, .Data = value, .Path = x.Name})
+            Return TypeDescriptor.GetProperties(value).Cast(Of PropertyDescriptor)().Select(Function(x) New Descriptor(x.Name, value, x))
         End Function
 
         Public Function ConvertBack(ByVal value As Object, ByVal targetType As Type, ByVal parameter As Object, ByVal culture As Globalization.CultureInfo) As Object Implements IValueConverter.ConvertBack
@@ -44,31 +43,37 @@ Namespace DXSample
     End Class
 
     Public Class Descriptor
-
-        Public Property Path As String
-
-        Public Property Data As Object
-
-        Public Property PropertyDescriptor As PropertyDescriptor
-
-        Public Property IsCollection As Boolean
+        Public ReadOnly Property Path As String
+        Public ReadOnly Property Data As Object
+        Public ReadOnly Property PropertyDescriptor As PropertyDescriptor
+        Public ReadOnly Property IsCollection As Boolean
+            Get
+                If PropertyDescriptor IsNot Nothing And Data IsNot Nothing Then
+                    Return TypeOf PropertyDescriptor.GetValue(Data) Is IEnumerable
+                End If
+                Return TypeOf Data Is IEnumerable
+            End Get
+        End Property
+        Public Sub New(path As String, data As Object, propertyDescriptor As PropertyDescriptor)
+            Me.Path = path
+            Me.Data = data
+            Me.PropertyDescriptor = propertyDescriptor
+        End Sub
     End Class
 
     Public Class PropertyDefinitionTemplateSelector
         Inherits DataTemplateSelector
 
         Public Property DefinitionTemplate As DataTemplate
-
         Public Property CollectionDefinitionTemplate As DataTemplate
-
         Public Property CustomDefinitionTemplate As DataTemplate
 
         Public Overrides Function SelectTemplate(ByVal item As Object, ByVal container As DependencyObject) As DataTemplate
             Dim descriptor As Descriptor = CType(item, Descriptor)
             Dim propertyDescriptor As PropertyDescriptor = descriptor.PropertyDescriptor
             If propertyDescriptor Is Nothing Then Return DefinitionTemplate
-            If descriptor.IsCollection Then Return CollectionDefinitionTemplate
             If propertyDescriptor.Attributes.Cast(Of Attribute)().Where(Function(n) TypeOf n Is CustomEditorAttribute).Any() Then Return CustomDefinitionTemplate
+            If descriptor.IsCollection Then Return CollectionDefinitionTemplate
             Return DefinitionTemplate
         End Function
     End Class
